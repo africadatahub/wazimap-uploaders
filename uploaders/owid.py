18c219abaddd4922e7cd6f039cafd6eb88fad219
JD Bothma <jbothma@gmail.com>
2021-05-21 05:50:41 +0200

    Add argparse

--- uploaders/owid.py
+++ uploaders/owid.py
@@ -2,14 +2,23 @@ import sys
 import requests
 import pandas as pd
 import numpy as np
+import argparse
 
-ENDPOINT = sys.argv[1]
-TOKEN = sys.argv[2]
+
 OWID_FILE_URL = "https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv"
 OWID_FILE_PATH = "/tmp/owid-covid-data.csv"
 WAZIMAP_FILE_PATH = "/tmp/owid_Vaccinations_DosesReceived.csv"
-DATASET_ID = sys.argv[3]
 
+parser = argparse.ArgumentParser(description='Upload data to Africa Data Hub wazimap')
+parser.add_argument("--wazimap-endpoint", type=str, required=True)
+parser.add_argument("--wazimap-token", type=str, required=True)
+
+group = parser.add_argument_group('Datasets')
+group.add_argument("--owid-vaccines-distributed-to-date", type=int, metavar="DATASET_ID")
+
+args = parser.parse_args()
+
+print(args)
 
 USE_COLS_POPULATION = ['iso_code', 'continent', 'date', 'stringency_index', 'population', 'population_density', 'median_age', 'aged_65_older', 'aged_70_older', 'gdp_per_capita',
                        'extreme_poverty', 'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers', 'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand',
@@ -17,7 +26,7 @@ USE_COLS_POPULATION = ['iso_code', 'continent', 'date', 'stringency_index', 'pop
                       'new_vaccinations', 'new_vaccinations_smoothed', 'total_vaccinations_per_hundred', 'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
                       'total_tests_per_thousand', 'new_tests_per_thousand', 'new_tests_smoothed_per_thousand', 'total_cases_per_million', 'total_deaths_per_million', 'positive_rate']
 
-
+exit(123)
 ## Fetch the OWID file
 owid_r = requests.get(OWID_FILE_URL, allow_redirects=True)
 owid_r.raise_for_status()
@@ -82,7 +91,7 @@ vaccinations_totals.to_csv(WAZIMAP_FILE_PATH, index = False, sep=',')
 
 
 ## Upload to wazimap
-url = f"{ENDPOINT}/api/v1/datasets/{DATASET_ID}/upload/"
+url = f"{args.wazimap_endpoint}/api/v1/datasets/{DATASET_ID}/upload/"
 
 headers = {'authorization': f"Token {TOKEN}"}
 files = {'file': open(WAZIMAP_FILE_PATH, 'rb')}
